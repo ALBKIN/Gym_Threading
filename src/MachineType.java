0310b4befc56b3971be6c995b7d0a4116be444db
ALBKIN <kinkel.albert@gmail.com>
2023-09-25 16:04:39 +0200

    Enum stores for different types of equipment

--- src/MachineType.java
+++ src/MachineType.java
@@ -1,2 +1,19 @@
-public class MachineType {
+import java.util.Random;
+
+// Enum of all gym equpiment
+public enum MachineType {
+    LEGPRESSMACHINE("Leg Press"), BARBELL("Bar Bells"), SQUATMACHINE("Squat Machine"), LEGEXTENSIONMACHINE("Leg Extensions"),
+    LEGCURLMACHINE("Leg Curls"), LATPULLDOWNMACHINE("Lat Pull Downs"), PECDECKMACHINE("Pec Deck Machine"),
+    CABLECROSSOVERMACHINE("Cable Crossovers");
+
+    public final String machineName;
+
+    MachineType(String machineName) {
+        this.machineName = machineName;
+    }
+
+    // Returns a random piece of gym equipment
+    public static MachineType getRandomEquipment() {
+        return MachineType.values()[new Random().nextInt(MachineType.values().length)];
+    }
 }
